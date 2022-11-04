df74cdc4e8b2cc19a300e74af8a31dbb559ac467
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-11-05 00:59:06 +0500

    main page reset

--- src/Pages/main/Main.jsx
+++ src/Pages/main/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import MainHeader from '../../components/mainHeader/MainHeader';
 
 const Main = () => {
@@ -14,18 +13,5 @@ const Main = () => {
     </>
   );
 };
-=======
-import Footer from '../../components/Footer/Footer';
-import MainHeader from '../../components/mainHeader/MainHeader';
-
-const Main = () => {
-    return (
-        <>
-            <MainHeader/>
-            <Footer/>
-        </>
-    );
-}
->>>>>>> acfd48a9ef12880043e954282ef538a1ae8ecddd
 
 export default Main;
