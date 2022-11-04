cc99dcde70752e710e9140adef2bc2fcbd756235
Bobur Hatamov <boburhatamov7@gmail.com>
2022-11-04 14:12:05 +0500

    header finished

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../mainHeader/mainHeader.css';
 import { NavLink } from "react-router-dom";
 import logo from '../../assets/images/logo.svg';
+import phone from '../../assets/images/header-phone-icon.svg';
 
 const MainHeader = () => {
   return (
@@ -9,7 +10,6 @@ const MainHeader = () => {
  <nav>
         <div className="main_container">
           <div className="main_navbar">
-            <div className="navbar_left-side">
               <NavLink to="/">
                 {" "}
                 <ul className="navbar_left_ul">
@@ -18,11 +18,25 @@ const MainHeader = () => {
                   </li>
                   <li className="nav_line"></li>
                   <li className="nav_logo_content">
-                  Экспертно-Правовое Агентство
+                  Экспертно-Правовое <br /> Агентство
                   </li>
                 </ul>
               </NavLink>
-            </div>
+            <ul  className="navbar_right_ul">
+              <li className='sign_in'>
+                     <button>Вход</button>
+              </li>
+              <li className='sign_up'>
+                     <button>Регистрация</button>
+              </li>
+              <li className='phone_comp'>
+                    <img src={phone} alt="" />
+                    <div className='phone_number'>
+                      <p>+7 (800) 600-85-89</p>
+                      <p>9:30 - 18:30 по МСК (ежедневно)</p>
+                    </div>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
