b80d6fb8fc8ace54f2b489c12e663916d4e3741f
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-11-07 12:52:22 +0500

    rating section finished

--- src/Pages/main/Main.jsx
+++ src/Pages/main/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import MainHeader from '../../components/mainHeader/MainHeader';
 import MainHero from '../../components/mainHero/mainHero';
-import './Main.css'
 import Footer from '../../components/Footer/Footer';
 import MainAside from '../../components/MainAside/MainAside';
+import MainOrder from '../../components/mainOrder/mainOrder';
+import MainRating from '../../components/mainRating/mainRating';
+import './Main.css'
+import MainFaceRating from '../../components/mainFaceRating/mainFaceRating';
 
 const Main = () => {
   return (
@@ -15,6 +18,21 @@ const Main = () => {
         <section className="hero">
           <div className="hero-container">
             <MainHero />
+          </div>
+        </section>
+        <section className="order">
+          <div className="order-container">
+            <MainOrder />
+          </div>
+        </section>
+        <section className="rating">
+          <div className="rating-container">
+            <MainRating />
+          </div>
+        </section>
+        <section className="rating">
+          <div className="rating-container">
+            <MainFaceRating />
           </div>
         </section>
       </main>
