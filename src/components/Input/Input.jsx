5a954a909dc6d2a08abb51713c08cd9ffeb83c37
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-11-08 13:09:13 +0500

    input component added

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,25 @@
+import React, { useState , useEffect } from 'react'
+import QuestionIcon from '../../assets/images/main/question.svg'
+import './Input.css'
+
+const Input = ({labelName , inputType , inputId , placeholder  , descValue , question}) => {
+    const [questionImage , setQuestionImage] = useState(question);
+    // console.log(question)
+    useEffect(() => {
+        question === true ? setQuestionImage(true) : setQuestionImage(false);
+    }, [question]);
+  return (
+    <div className='input-wrapper'>
+        <div className="label-wrapper">
+            <label className='label' htmlFor={inputId}>{labelName}</label>
+            {
+                questionImage ? <img src={QuestionIcon} alt="question" /> : ''
+            }
+        </div>
+        <input type={inputType} id={inputId} placeholder={placeholder} className='input' />
+        <p className='desc'>{descValue}</p>
+    </div>
+  )
+}
+
+export default Input
