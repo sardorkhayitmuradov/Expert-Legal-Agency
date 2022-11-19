454e202853f871e385e6d57657872c829bb8dc12
Bobur Hatamov <boburhatamov7@gmail.com>
2022-11-19 17:23:36 +0500

    modal fixed and register api ishlavotti

--- src/App.js
+++ src/App.js
@@ -22,32 +22,36 @@ import Search from "./Pages/search/Search";
 import Services from "./Pages/services/Services";
 import Setting from "./Pages/setting/Setting";
 
-
 function App() {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/code' element={<Code/>}/>
-          <Route path="/emailcode" element={<CodeEmail />} />
-          <Route path='/email' element={<Email />} />
-          <Route path='/form1' element={<Form1 />} />
-          <Route path='/form2' element={<Form2 />} />
-          <Route path="/fqa" element={<Fqa/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/newpassword" element={<NewPassword/>} />
-          <Route path="/ordernew" element={<OrderNew/>} />
-          <Route path='/password' element={<Password/>}/>
-          <Route path='/personal' element={<Personal/>}/>
-          <Route path='/profile1' element={<Profile1/>} />
-          <Route path="/profile2" element={<Profile2/>} />
-          <Route path="/registration1" element={<Registration1/>} />
-          <Route path="/registration2" element={<Registration2/>} />
-          <Route path='/search' element={<Search/>}/>
-          <Route path='/services' element={<Services/>}/>
-          <Route path='/setting' element={<Setting/>} />
-          <Route path='*' element={<Error />} />
+        <Route path="/" element={<Main open={open} setOpen={setOpen} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/code" element={<Code />} />
+        <Route path="/emailcode" element={<CodeEmail />} />
+        <Route path="/email" element={<Email />} />
+        <Route path="/form1" element={<Form1 />} />
+        <Route path="/form2" element={<Form2 />} />
+        <Route path="/fqa" element={<Fqa />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/ordernew" element={<OrderNew />} />
+        <Route path="/password" element={<Password />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/profile1" element={<Profile1 />} />
+        <Route path="/profile2" element={<Profile2 />} />
+        <Route
+          path="/registration1"
+          element={<Registration1 setOpen={setOpen} />}
+        />
+        <Route path="/registration2" element={<Registration2 />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
