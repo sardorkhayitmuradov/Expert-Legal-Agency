1270f7a7b1cfb47eda667d3370327af7fdae85db
Bobur Hatamov <boburhatamov7@gmail.com>
2022-11-22 13:51:12 +0500

    ordernew page finished

--- src/Pages/orderNew/OrderNew.jsx
+++ src/Pages/orderNew/OrderNew.jsx
@@ -4,7 +4,8 @@ import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
 import Footer from "../../components/Footer/Footer";
 import "../../Pages/orderNew/OrderNew.css";
 import vector from "../../assets/images/main/Vector.svg";
-import statusTimeIcon from '../../assets/images/main/statusTime.svg'
+import statusTimeIcon from "../../assets/images/main/statusTime.svg";
+import Input from "../../components/Input/Input";
 
 const OrderNew = () => {
   const navigate = useNavigate("");
@@ -36,8 +37,8 @@ const OrderNew = () => {
           </button>
         </div>
         <div className="order__new__content">
-        <div className="order__new__details">
-          <h5>Детали заказа</h5>
+          <div className="order__new__details">
+            <h5>Детали заказа</h5>
             <ul>
               <li className="types">Создан:</li>
               <li className="back__type_1">12.02.2022</li>
@@ -57,9 +58,115 @@ const OrderNew = () => {
             <h4 className="order__details__h4">Скачать файлы</h4>
             <p className="order__details__p">Чек оплаты</p>
             <ul>
-              <li><img src={statusTimeIcon} alt="Status-Time" loading="lazy"/></li>
-              <li className="order__details__soon">Здесь скоро появятся <br /> результаты оценки</li>
+              <li>
+                <img src={statusTimeIcon} alt="Status-Time" loading="lazy" />
+              </li>
+              <li className="order__details__soon">
+                Здесь скоро появятся <br /> результаты оценки
+              </li>
             </ul>
+          </div>
+          <div className="order__new__inputs">
+            <h5>Информация о заказчике</h5>
+            <form className="order__new__inputs1">
+              <Input
+                labelName={"Фамилия"}
+                inputType={"text"}
+                inputId={"userFirstName"}
+                placeholder={"Петров"}
+                question={false}
+              />
+              <Input
+                labelName={"Имя"}
+                inputType={"text"}
+                inputId={"userName"}
+                placeholder={"Максим"}
+                question={false}
+              />
+              <Input
+                labelName={"Отчество"}
+                inputType={"text"}
+                inputId={"userSecondName"}
+                placeholder={"Александрович"}
+                question={false}
+              />
+              <Input
+                labelName={"Email"}
+                inputType={"email"}
+                inputId={"userEmail"}
+                placeholder={"example@gmail.com"}
+                question={true}
+              />
+              <Input
+                labelName={"Номер телефона"}
+                inputType={"tel"}
+                inputId={"userPhone"}
+                placeholder={"+7 (900) 000-00-00"}
+                question={false}
+              />
+              <Input
+                labelName={"Место проживания"}
+                inputType={"text"}
+                inputId={"userAddress"}
+                placeholder={"Москва"}
+                question={true}
+              />
+            </form>
+            <h5>Информация о собственнике</h5>
+            <form className="order__new__inputs2">
+              <Input
+              labelName={"ФИО собственника"}
+              inputType={"text"}
+              inputId={"userFullName"}
+              placeholder={"Иванов Иван Иванович"}
+              question={true}
+              />
+               <Input
+              labelName={"Дата смерти собственника"}
+              inputType={"date"}
+              inputId={"userDieDate"}
+              placeholder={"22/12/2020"}
+              question={false}
+              />
+              <Input
+              labelName={"Дата смерти собственника"}
+              inputType={"text"}
+              inputId={"userDiePassport"}
+              placeholder={"CT №550657"}
+              question={false}
+              />
+            </form>
+            <h5>Информация об автомобиле</h5>
+            <form className="order__new__inputs3">
+            <Input
+              labelName={"VIN"}
+              inputType={"text"}
+              inputId={"userCarVin"}
+              placeholder={"4567890987NG"}
+              question={true}
+              />
+               <Input
+              labelName={"Марка и модель автомобиля"}
+              inputType={"text"}
+              inputId={"userCardMark"}
+              placeholder={"Ba3 (LADA) Priora"}
+              question={true}
+              />
+               <Input
+              labelName={"Год выпуска"}
+              inputType={"text"}
+              inputId={"userCardYear"}
+              placeholder={"1950"}
+              question={true}
+              />
+               <Input
+              labelName={"Место нотариальных действий"}
+              inputType={"text"}
+              inputId={"userNotaialPlace"}
+              placeholder={"Москва"}
+              question={true}
+              />
+            </form>
           </div>
         </div>
       </div>
