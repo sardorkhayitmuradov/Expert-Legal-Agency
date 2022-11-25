8ec9d887dc4adea598d4704231fa2084c8dd1611
Bobur Hatamov <boburhatamov7@gmail.com>
2022-11-25 09:51:47 +0500

    products api worked and mainFaceRacing filter finished

--- src/components/mainFaceRating/mainFaceRating.jsx
+++ src/components/mainFaceRating/mainFaceRating.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import flatIcon from "../../assets/images/main/flat.svg";
-import landPilotIcon from "../../assets/images/main/land plot.svg";
-import houseIcon from "../../assets/images/main/house.svg";
-import garageIcon from "../../assets/images/main/garage.svg";
-import houseWithPilot from "../../assets/images/main/house-with-land-plot.svg";
-import wareHouseIcon from "../../assets/images/main/warehouse.svg";
+// import flatIcon from "../../assets/images/main/flat.svg";
+// import landPilotIcon from "../../assets/images/main/land plot.svg";
+// import houseIcon from "../../assets/images/main/house.svg";
+// import garageIcon from "../../assets/images/main/garage.svg";
+// import houseWithPilot from "../../assets/images/main/house-with-land-plot.svg";
+// import wareHouseIcon from "../../assets/images/main/warehouse.svg";
 import timeIcon from "../../assets/images/main/time.svg";
 import { useNavigate } from "react-router-dom";
 import "./mainFaceRating.css";
@@ -13,42 +13,60 @@ import axios from "axios";
 const MainFaceRating = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
+  let url = "http://epa.yarbek.uz/api/products/";
+
+  function filterData(assets = "") {
     axios
-      .get("http://epa.yarbek.uz/api/products/")
+      .get(`${url}${assets}`)
       .then((res) => {
         setData(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
+  }
+  useEffect(() => {
+    filterData("");
   }, []);
-  console.log(data.results);
-  
-
-  const filterTypes = (categoryItem) => {
-    const result = data.results.filter((el) => {
-      return (el.name = categoryItem);
-    });
-    console.log(result);
-  };
-
 
   const navigate = useNavigate();
   return (
     <>
       <h2 className="rating__title">ЗАКАЗАТЬ ОЦЕНКУ</h2>
-            <div className="rating__nav">
-            <p className="rating__nav__title">Недвижимое имущество</p>
-            <p className="rating__nav__title">Активы и аренда</p>
-            <p className="rating__nav__title">Движимое имущество</p>
+      <div className="rating__nav">
+      <p
+          className="rating__nav__title"
+          onClick={() => filterData()}
+        >
+          Все
+        </p>
+        <p
+          className="rating__nav__title"
+          onClick={() => filterData("?type_assessments=1")}
+        >
+          Недвижимое имущество
+        </p>
+        <p
+          className="rating__nav__title"
+          onClick={() => filterData("?type_assessments=2")}
+        >
+          Активы и аренда
+        </p>
+        <p
+          className="rating__nav__title"
+          onClick={() => filterData("?type_assessments=3")}
+        >
+          Движимое имущество
+        </p>
       </div>
       <ul className="rating__list">
-        <li className="rating__item">
-          <img src={flatIcon} alt="" className="rating__item__image" />
+       {data?.results?.map((el) => {
+        return(
+          <li className="rating__item" key={el.id}>
+          <img src={el.img} alt="" className="rating__item__image" />
 
           <div className="rating__image__info">
-            <h4 className="rating__item__title">Легковой автомобиль</h4>
+            <h4 className="rating__item__title">{el.product_name}</h4>
             <span className="rating__info__wrapper">
               <img
                 src={timeIcon}
@@ -57,10 +75,10 @@ const MainFaceRating = () => {
                 height="24px"
                 style={{ marginRight: "6px" }}
               />{" "}
-              Срок изготовления — 1 час
+             {el.desc}
             </span>
             <div className="rating__item__bottom">
-              <p className="rating__item__price">1100₽</p>
+              <p className="rating__item__price">{el.price}₽</p>
               <button
                 className="rating__item__button"
                 onClick={() => navigate("/form1")}
@@ -70,7 +88,9 @@ const MainFaceRating = () => {
             </div>
           </div>
         </li>
-        <li className="rating__item">
+        )
+       })}
+        {/* <li className="rating__item">
           <img src={garageIcon} alt="" className="rating__item__image" />
           <div className="rating__image__info">
             <h4 className="rating__item__title">Акции</h4>
@@ -197,7 +217,7 @@ const MainFaceRating = () => {
               </button>
             </div>
           </div>
-        </li>
+        </li> */}
       </ul>
     </>
   );
